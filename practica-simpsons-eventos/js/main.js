6c397d253a0246cc24aa125d83d8e9ea5639eb52
JoacoTitievsky <132691909+JoacoTitievsky@users.noreply.github.com>
2023-05-19 17:23:56 -0300

    Add files via upload

---
+++ practica-simpsons-eventos/js/main.js
@@ -0,0 +1,29 @@
+// completar las variables para que el programa funcione
+
+window.addEventListener('load', function() {
+
+    //capturar HTML 
+    let titulo = document.querySelector("#titulo");
+    titulo.addEventListener("mouseover", function(){
+        let nombre = prompt("¿CÓMO TE LLAMÁS?")
+        if (nombre.length < 1){
+            alert("BIENVENID@")
+        }
+        else{
+            alert("BIENVENID@ USUARIO")
+        }
+    })
+ 
+    /* let saludo = ;
+ 
+    let span = ; */
+ 
+    /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
+    así como también su funcionalidad */
+    
+
+    /* Asignarle evento a la variable boton */
+   /*  let boton = ; */
+ 
+ 
+ }) 
